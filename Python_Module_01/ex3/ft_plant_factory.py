f5cf008e19bfe239b94cdf2a231f42eb99f51406
Sara Fontes <sfontes94@gmail.com>
2026-01-26 22:38:39 +0000

    added Module 01

---
+++ Python_Module_01/ex3/ft_plant_factory.py
@@ -0,0 +1,7 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        """Initialize a Plant instance."""
+        self.name = name
+        self.height = height
+        self.age_days = age
+

3ffa1786fd6af8fe25738301d4a114ca2902a7a4
Sara Fontes <sfontes94@gmail.com>
2026-04-13 15:23:51 +0100

    added ex8

---
+++ Python_Module_08/ex0/construct.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import site
+
+
+def construct() -> None:
+    """
+    • Detects whether it is running inside a virtual environment
+    • Displays information about the current Python environment
+    • Provides instructions for creating and activating a
+    virtual environment if none isdetected
+    • Shows the difference between global and virtual
+    environment package locations
+    -> works both inside and outside virtual environments,
+    providing different outputs for each scenario
+    -> Creating and testing virtual environment
+            $> python3 -m venv matrix_env
+            $> source matrix_env/bin/activate
+            (matrix_env) $> python3 construct.py
+            Should detect virtual environment and show details
+    """
+    v_path = os.environ.get('VIRTUAL_ENV')
+
+    if v_path:
+        print("\nMATRIX STATUS: Welcome to the construct\n"
+              f"Current Python: {sys.executable}\n"
+              f"Virtual Environment: {os.path.basename(v_path)}\n"
+              f"Environment Path: {v_path}\n"
+              "SUCCESS: You're in an isolated environment!\n"
+              "Safe to install packages without affecting\n"
+              "the global system.\n"
+              f"Package installation path: {site.getsitepackages()[0]}")
+    else:
+        print("\nMATRIX STATUS: You're still plugged in\n")
+        print(f"Current Python: {sys.executable}")
+        print("Virtual Environment: None detected\n\n"
+              "WARNING: You're in the global environment!\n"
+              "The machines can see everything you install.\n\n"
+              "To enter the construct, run:\n"
+              "python -m venv matrix_env\n"
+              "source matrix_env/bin/activate # On Unix\n"
+              "matrix_env\\Scripts\\activate # On Windows\n\n"
+              "Then run this program again.")
+
+
+if __name__ == "__main__":
+    construct()
